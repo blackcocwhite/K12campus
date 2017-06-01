f885e2f2fd76639530d93751080942099f789e40
hengjiang <80764426@qq.com>
2017-06-01 17:01:15 +0800

    fix cors in different env

---
+++ config/cors.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Laravel CORS
+     |--------------------------------------------------------------------------
+     |
+     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+     | to accept any value.
+     |
+     */
+    'supportsCredentials' => false,
+    'allowedOrigins' => [env('APP_ROOT')],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+];
+
